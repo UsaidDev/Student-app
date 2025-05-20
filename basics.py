e4202eb4f0c3cc3ed2d16ad78f0fe2215920baa4
Usaid <usaidokdev@gmail.com>
2025-05-20 14:57:53 +0530

    Intial commit

---
+++ basics.py
@@ -0,0 +1,57 @@
+from tabulate import tabulate
+
+def student_detail():
+
+    student=input("Enter your name🎉:")
+    if not student.strip().replace(" ", "").isalpha():
+        print("❌ Name should have only letters")
+        student=input("Enter your name again❗️: ")
+        if not student.strip().isalpha():
+            print("❌ Invalid again, stopping now⚠️")
+            return None
+        
+    age=input(str("Age 👍:"))
+    if not age.isdigit():
+        print("❌ Age should be a number")
+        age=input(str("Enter your Age Again🔄:"))
+        if not age.isdigit():
+            print("Invalid again, stopping now🚫")
+            return None
+        
+    location=input("Location📍:")
+    if not location.strip().replace(" ", "").isalpha():
+        print("❌ Location should have only letters")
+        location=input("Enter your location again⏳:")
+        if not location.strip():
+            print("Invalid again, stopping now❗️")
+            return None
+    return student,age,location
+
+def process_data(): # Do next task's
+    details=student_detail()
+    if details:
+       student,age,location=details
+       print("\n"+student+"\n"+age+"\n"+location+"\n")
+       
+       return None
+    else:
+       print("Enterd value is invalid")
+       return None
+
+def nearby_school():
+    print("Please select nearby school")
+    print("1:-Loyola School")
+    print("2:-Kendriya Vidyalaya school")
+    print("3:-Arya Central School")
+    choise=input('Please select the school number:')
+    return choise
+
+def subject_selection():
+    print("Please Select the Subject:")
+    print("1:Science")
+    print("2:Commerce")
+    print("3:Humanities")
+    subject=input("Please select the subject number:")
+    return subject
+
+process_data()
